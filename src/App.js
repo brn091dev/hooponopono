8c1a070a52e6e1705aa2f6d83f1c9b8a3d1bd279
Brayan <brn091dev@gmail.com>
2021-10-22 20:13:16 -0500

    github pages cero counter

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
 
-  const [number, setNumber] = useState(1)
+  const [number, setNumber] = useState(0)
   const msgs = ['Lo siento', 'Perdóname', 'Gracias', 'Te amo']
   const [msgToShow, setMsgToShow] = useState('')
   const [state, setState] = useState(false)
@@ -29,17 +29,23 @@ function App() {
     // <section className="App">
     <section class="w-screen h-screen p-16 flex flex-col justify-between items-center bg-primary">
       <div class="divide-y divide-gray-400">
-        <p class="text-6xl font-black text-red-500 mb-5 text-center">HO'OPONOPONO</p>
-        <h1 class="text-4xl pt-10 text-indigo-100 text-center">{number}</h1>
+        <p class="text-lg lg:text-4xl xl:text-6xl font-black text-red-500 mb-5 text-center">HO'OPONOPONO</p>
+        {
+          number !== 0 &&
+          <h1 class="text-4xl pt-10 text-indigo-100 text-center">{number}</h1>
+        }
       </div>
-      <div className="h-40">
+      <div className="h-30">
         <p class="text-5xl font-black  text-indigo-100">{msgToShow}</p>
       </div>
-      <svg class="animate-bounce w-6 h-6 text-white mt-4 mb-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-        <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
-      </svg>
+      {
+        number === 0 &&
+        <svg class="animate-bounce w-6 h-6 text-white mt-4 mb-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+          <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+        </svg>
+      }
       <button
-        class="p-2 pl-5 pr-5 bg-transparent border-2 border-red-500 text-red-500 text-lg rounded-lg hover:bg-red-500 hover:text-gray-100 focus:border-4 focus:border-red-300"
+        class="mb-10 p-2 pl-5 pr-5 bg-transparent border-2 border-red-500 text-red-500 text-lg rounded-lg hover:bg-red-500 hover:text-gray-100 focus:border-4 focus:border-red-300"
         onClick={() => {
           setState(!state)
         }}
